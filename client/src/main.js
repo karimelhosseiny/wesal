9cd83d17fd9e9393d622e401b79bf5f36333424d
YoussefAbdulaziz <azizy5203@gmail.com>
2022-03-18 01:07:42 +0200

    created login page

---
+++ client/src/main.js
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.css";
+
+import { createApp } from "vue";
+
+import App from "./App.vue";
+
+createApp(App).mount("#app");
+
+import "bootstrap/dist/js/bootstrap.js";
