5400c381cebf763e35442235bbea5e3fba55d744
Karim <sim.karime.ahmed@alexu.edu.eg>
2022-04-22 01:33:27 +0200

    just use DB

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,7 @@ use App\Models\Organization;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Validator;
 
 class UserController extends Controller
