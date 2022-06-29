7bd5551d981213d36cc291d60798739c8a82a326
mohamedmoustafa612 <mido.moustafa612@gmail.com>
2022-06-29 20:44:41 +0200

    Working on reminders

--- client/src/components/local/ReminderCard.vue
+++ client/src/components/local/ReminderCard.vue
@@ -1,16 +1,10 @@
 <script>
 export default {
-    props: [
-        "id",
-        "title",
-        'totalCases',
-        'totalDonations',
-        'totalDonors',
-        "pic",
-    ],
+    props: ["pic", "title", "org", "id", "caseDesc","goal", "raised","isFavorite", "reminder","cat"],
     emits: ["toggle-favorite", "toggle-reminder"],
     data() {
-        return {};
+        return {
+        };
     },
     methods: {
         ChangingHeartIcons() {
@@ -24,108 +18,145 @@ export default {
 </script>
 
 <template>
-    <div v-if="title!=''|| title==null" class="card p-2 container">
+    <div class="card p-2">
         <div class="top">
-            <img
-                src="../../assets/7maya.png"
-                class="mx-2 mt-2"
-                alt="Organization image"
-            />
-            <a class="org mt-2">{{title}}</a>
-        </div>
-        <div class="mid container row justify-content-center">
-            <div class="row">
-                <span class="target col-5">Total cases:</span>
-                <span class="col-6">
-                    {{totalCases}}
-                    <sub>Case</sub>
-                </span>
-                <div class="row">
-                    <span class="target col-7">Total donations:</span>
-                    <span class="col-5">
-                        {{totalDonations}}
-                        <sub>EGP</sub>
-                    </span>
-                </div>
-                <div class="row">
-                    <span class="target col-6">Total donors:</span>
-                    <span class="col-6">
-                        {{totalDonors}}
-                        <sub>Donor</sub>
-                    </span>
-                </div>
+            <img src="../../assets/7maya.png" class="ms-3" alt="..." />
+            <div class="title ms-2 mt-3">
+                <span class="title">{{ title }}</span>
+                <a class="org h6 fw-normal">{{ org }}</a>
+            </div>
+            <div class="icons">
+                <i
+                    @click="ChangingHeartIcons()"
+                    :class="
+                        isFavorite
+                            ? 'bi bi-heart-fill text-success'
+                            : 'bi bi-heart'
+                    "
+                ></i>
+                <i
+                    @click="ChangingcalendertIcons()"
+                    :class="
+                        reminder
+                            ? 'bi bi-calendar-check text-success'
+                            : 'bi bi-calendar'
+                    "
+                ></i>
             </div>
         </div>
-        <div class="bottom row justify-content-center">
+        <div class="mid">
+            <p class="descr fw-bold">
+                {{ caseDesc }}
+            </p>
+            <span class="target">target:</span>
+            <span>
+                {{' ' + goal }}
+                <sub>egp</sub>
+            </span>
+            <div class="progressBar">
+                <div class="ammount mt-3 px-2 d-flex justify-content-between">
+                    <span>
+                        {{ raised }}
+                        <sub>egp</sub>
+                    </span>
+                    <span>
+                        {{ goal }}
+                        <sub>egp</sub>
+                    </span>
+                </div>
+                <progress
+                    class="bar bg-transparent"
+                    :value= "raised > 0 ? raised : 0"
+                    :max= "goal"
+                ></progress>
+            </div>
+        </div>
+        <div class="bottom">
             <router-link
-                :to=" '/organization/' + this.id"
-                class="col-6 btn btn-block more rounded-pill text-decoration-none text-light"
-                >Visit Profile</router-link
+                :to=" '/casepage/' + this.id"
+                class="btn btn-block more rounded-pill text-decoration-none text-light"
+                >More</router-link
             >
         </div>
     </div>
-    <div v-else class="text-center my-5">
-                <div
-                    class="spinner-grow mx-5 text-success"
-                    style="width: 10rem; height: 10rem"
-                    role="status"
-                >
-                    <span class="visually-hidden">Loading...</span>
-                </div>
-            </div>
 </template>
 
 <style lang="scss" scoped>
 @use "../../sass/colors" as *;
 
-$logoSize: 80px;
+$logoSize: 60px;
 .card {
-    width: 380px;
-    height: 350px;
-    margin: 20px;
+    width: 310px;
+    height: 364px;
     outline: none;
     border: none;
     background: white;
     border-radius: 22px;
     box-shadow: 0px 0px 21px rgba($color: #000000, $alpha: 0.2);
-
+    display: grid;
+    grid-template-rows: 20% 60% 20%;
+    grid-template-areas:
+        "top"
+        "mid"
+        "bottom";
     .top {
         display: grid;
-        gap: 40px;
-        grid-template-columns: 20% 80%;
-        justify-content: space-between;
+        grid-template-columns: 1fr 2fr 1fr;
+        grid-area: "top";
         img {
-            grid-column: 1;
             width: $logoSize;
             height: $logoSize;
             border-radius: 50%;
+            justify-self: start;
+            align-self: center;
+
             border: 2px solid $priColor;
         }
-
-        .org {
-            grid-column: 2;
-            width: fit-content;
-            text-decoration: none;
-            font-size: 30px;
-            font-weight: 600;
+        .title {
+            justify-self: start;
+            align-self: start;
+            display: flex;
+            flex-direction: column;
+            gap: 0;
             color: $priColor;
+            .title {
+                font-size: 15px;
+                font-weight: 800;
+            }
+            .org {
+                width: fit-content;
+                font-size: 15px;
+                text-decoration: none;
+                color: $priColor;
+                cursor: pointer;
+                &:hover {
+                    text-decoration: underline;
+                }
+            }
+        }
+        .icons {
+            color: $priColor;
+            align-self: center;
+            justify-self: center;
+            font-size: 20px;
             cursor: pointer;
-            &:hover {
-                text-decoration: underline;
+
+            i {
+                display: block;
             }
         }
     }
     .mid {
-        margin-top: 10px;
+        grid-area: "mid";
         padding: 1.3em;
         color: $priColor;
         p {
             width: 252.5px;
             height: 50px;
             color: black;
-            white-space: nowrap;
-            overflow: hidden;
-            text-overflow: ellipsis;
+            white-space: nowrap ;
+            overflow: hidden ;
+            text-overflow: ellipsis ;
         }
 
         .target {
@@ -134,17 +165,32 @@ $logoSize: 80px;
         }
 
         span {
-            font-weight: 800;
+            font-weight: 600;
+        }
+        .progressBar {
+            .bar[value]::-webkit-progress-bar {
+                height: 10px;
+                width: 160%;
+                border-radius: 10px;
+                margin-top: 7px;
+            }
+            .bar[value]::-webkit-progress-value {
+                background-color: $priColor;
+                border-radius: 10px;
+            }
         }
     }
     .bottom {
-        margin-left: 2px;
+        grid-area: "bottom";
+        align-self: start;
+        display: grid;
         max-width: 100%;
         overflow: hidden;
         margin-top: 0.5em;
         .more {
             background-color: $priColor;
             width: 60%;
+            justify-self: center;
             font-weight: bold;
             font-size: 24px;
             color: $white;
