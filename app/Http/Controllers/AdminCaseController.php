a75f86533df5a064440960c357ff4b34f4eaf063
mohamedmoustafa612 <mido.moustafa612@gmail.com>
2022-07-03 05:30:05 +0200

    admin case controller is fixed now

--- app/Http/Controllers/AdminCaseController.php
+++ app/Http/Controllers/AdminCaseController.php
@@ -19,21 +19,21 @@ class AdminCaseController extends Controller
 {
     //admin add case
     public function adminaddcase(Request $request){
-        if ($request->hasfile('image')) {
-            $request->validate(
-                     [
-                         'image' => 'image|mimes:jpeg,png,jpg,gif,svg|',
-                     ]
-                 );
-            //upload image
-            $path=cloudinary()->upload($request->file('image')->getRealPath(),$options=["folder"=>"images"])->getSecurePath();
-        }
+        // if ($request->hasfile('image')) {
+        //     $request->validate(
+        //              [
+        //                  'image' => 'image|mimes:jpeg,png,jpg,gif,svg|',
+        //              ]
+        //          );
+        //     //upload image
+        //     $path=cloudinary()->upload($request->file('image')->getRealPath(),$options=["folder"=>"images"])->getSecurePath();
+        //}
             $date = new DateTime();
             DB::table('donation_cases')->insert([
                     'title' =>$request->input('title'),
                     'goal_amount' =>$request->input('goal'),
                     'raised_amount' =>0,
-                    'image' => $path,
+                    //'image' => $path,
                     'deadline' => $request->input('deadline'),
                     'description' => $request->input('description'),
                     'organization_id' =>$request->input('org'),
@@ -42,33 +42,33 @@ class AdminCaseController extends Controller
                     'created_at' =>$date->format('Y-m-d H:i:s'),
                     'updated_at'=>$date->format('Y-m-d H:i:s'),
                  ]);
-        
+
             return response()->json([
                 'message' => 'Case added successfully',
             ], 200);
     }
     //admin update case
     public function adminupdatecase(Request $request){
-        if ($request->hasfile('image')) {
-            $request->validate(
-                     [
-                         'image' => 'image|mimes:jpeg,png,jpg,gif,svg|',
-                     ]
-                 );
-            //upload image
-            $path=cloudinary()->upload($request->file('image')->getRealPath(),$options=["folder"=>"images"])->getSecurePath();
-        }
+        // if ($request->hasfile('image')) {
+        //     $request->validate(
+        //              [
+        //                  'image' => 'image|mimes:jpeg,png,jpg,gif,svg|',
+        //              ]
+        //          );
+        //     //upload image
+        //     $path=cloudinary()->upload($request->file('image')->getRealPath(),$options=["folder"=>"images"])->getSecurePath();
+        //}
             $case = DonationCase::find($request->input('case_id'));
             $case->title = $request->input('title');
             $case->goal_amount = $request->input('goal');
             $case->raised_amount = $request->input('raised');
-            $case->image =$path;
+            //$case->image =$path;
             // $case->deadline = $request->input('deadline');
             // $case->description = $request->input('description');
             // $case->organization_id = $request->input('organizationid');
             $case->category_id = $request->input('category');
             $case->save();
-        
+
             return response()->json([
                 'message' => 'Case updated successfully',
             ], 200);
